bbe2a13228b62acf2ba3982621b6c4dab79c4ebb
leandro <leandro.andrekowicz@gmail.com>
2025-09-10 19:43:46 -0300

    Inicio do Projeto

---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "leandro"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,14 @@
+package br.com.api
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureDatabases()
+    configureHTTP()
+    configureRouting()
+}
